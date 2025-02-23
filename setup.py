c43a3e4e94d2b375c0a28c54d876d339d4785310
Gaurav Anand <155150251+GauravAnand30@users.noreply.github.com>
2025-02-24 00:10:31 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'RAG Project',
+    version= '0.0.0',
+    author= 'Gaurav Anand',
+    author_email= 'royygaurav@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
